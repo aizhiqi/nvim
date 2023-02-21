643006719c49b3a03b353ad1ec509366f5d8c181
aizhiqi <zhiqi.ai@gmail.com>
2023-02-21 12:56:22 +0800

    auto load plugin for lua\cpp\json

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -28,43 +28,89 @@ autocmd("BufEnter", {
 })
 
 
+
+local _plugins_loaded = {}
+local function _Set (list)
+  local set = {}
+  for _, l in ipairs(list) do set[l] = true end
+  return set
+end
+
+
 autocmd("FileType", {
   group = myAutoGroup,
-  pattern = "*.json",
+  pattern = "*",
   callback = function()
-    require("schemastore").setup({
-      vim.notify("schemastore loaded")
-    })
-  end,
+    if _Set(_plugins_loaded)[vim.bo.filetype] then
+      return
+    end
+
+    table.insert(_plugins_loaded, vim.bo.filetype)
+
+    if _Set({"h", "hpp", "cc", "c", "cpp" })[vim.bo.filetype]  then
+
+    elseif _Set({"lua"})[vim.bo.filetype] then
+      require("neodev").setup({
+        vim.notify("neodev auto loaded")
+      })
+      --[[ vim.lsp.start({
+        name = "lua-language-server",
+        cmd = { "lua-language-server" },
+        before_init = require("neodev.lsp").before_init,
+        root_dir = vim.fn.getcwd(),
+        settings = { Lua = {} },
+      }) ]]
+
+    elseif _Set({"json"})[vim.bo.filetype] then
+      require("schemastore").setup({
+        vim.notify("schemastore auto loaded")
+      })
+
+    elseif _Set({"rs"})[vim.bo.filetype] then
+      require("rust-tools").setup({
+        vim.notify("rust-tools auto loaded")
+      })
+
+    else
+
+    end
+
+
+  end
 })
-autocmd("FileType", {
-  group = myAutoGroup,
-  pattern = "*.rs",
-  callback = function()
-    require("rust-tools").setup({
-      vim.notify("rust-tools loaded")
-    })
-  end,
-})
-autocmd("FileType", {
-  group = myAutoGroup,
-  pattern = { "*.h", "*.hpp", "*.cc", "*.c", "*.cpp" },
-  callback = function()
-  end,
-})
-autocmd("FileType", {
-  group = myAutoGroup,
-  pattern = "*.lua",
-  callback = function()
-    require("neodev").setup({
-      vim.notify("neodev loaded")
-    })
-    --[[ vim.lsp.start({
-      name = "lua-language-server",
-      cmd = { "lua-language-server" },
-      before_init = require("neodev.lsp").before_init,
-      root_dir = vim.fn.getcwd(),
-      settings = { Lua = {} },
-    }) ]]
-  end,
-})
+
+--[[ autocmd("FileType", {
+--  group = myAutoGroup,
+--  pattern = "*.json",
+--  callback = function()
+--    require("schemastore").setup({
+--      vim.notify("schemastore loaded")
+--    })
+--  end,
+--})
+--autocmd("FileType", {
+--  group = myAutoGroup,
+--  pattern = "*.rs",
+--  callback = function()
+--    require("rust-tools").setup({
+--      vim.notify("rust-tools loaded")
+--    })
+--  end,
+--})
+--autocmd("FileType", {
+--  group = myAutoGroup,
+--  pattern = "*.lua",
+--  callback = function()
+--    require("neodev").setup({
+--      vim.notify("neodev loaded")
+--    })
+--    -[[ vim.lsp.start({
+--      name = "lua-language-server",
+--      cmd = { "lua-language-server" },
+--      before_init = require("neodev.lsp").before_init,
+--      root_dir = vim.fn.getcwd(),
+--      settings = { Lua = {} },
+--    }) ]]
+--  end,
+--})
+
