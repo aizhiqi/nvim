2e87b5b29ffa3939ab4698b2deb3815a32aca453
aizhiqi <zhiqi.ai@gmail.com>
2023-03-13 16:36:18 +0800

    adjust keybindings

--- lua/tools/toggleterm.lua
+++ lua/tools/toggleterm.lua
@@ -16,6 +16,7 @@ toggleterm.setup({
   on_open = function(term)
     vim.cmd("startinsert!")
     vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<ESC>", "<C-\\><C-n>", {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
   end,
   -- function to run on closing the terminal
   on_close = function(term)
